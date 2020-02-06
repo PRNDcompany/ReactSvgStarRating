63c9a574814ffa5c63d4d3404446b0c70b3177cb
nick-prnd <nick@prnd.co.kr>
2020-02-06 12:04:00 +0900

    Set demo site

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,14 @@
-module.exports = process.env.NODE_ENV === 'production' ? require('./config/webpack.prod.config') : require('./config/webpack.dev.config');
+let config;
+switch (process.env.NODE_ENV) {
+  case 'production':
+    config = require('./config/webpack.prod.config');
+     break;
+  case 'development':
+    config = require('./config/webpack.dev.config');
+    break;
+  case 'site':
+    config = require('./config/webpack.site.config');
+    break;
+}
+module.exports = config;
+
